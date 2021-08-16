4e5f9f5e1b8c93be34daaafa49bdfc857122370d
Amir Iranmanesh <iranmanesh.ah@gmail.com>
2021-08-16 21:13:29 +0430

    fix workflow

--- src/portfolio.js
+++ src/portfolio.js
@@ -316,7 +316,7 @@ const contactPageData = {
     title: "Blogs",
     subtitle:
       "I don't blog frequently but when I do something awesome, I do try to document it so it can be helpful to others. I write on Twitter.",
-    link: "https://twitter.com/Harikrushn9",
+    link: "",
     avatar_image_path: "blogs_image.svg",
   },
 };
