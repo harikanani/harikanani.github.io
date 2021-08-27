8dc2c1461a516f0e48c99597d9f21404f836afe0
Amir Iranmanesh <iranmanesh.ah@gmail.com>
2021-08-27 19:49:20 +0430

    add torob project

--- src/portfolio.js
+++ src/portfolio.js
@@ -14,10 +14,8 @@ const greeting = {
   logo_name: "AMiR",
   nickname: "amiranmanesh",
   full_name: "Amirhossein Iranmanesh",
-  subTitle:
-    "Expert Mobile Developer and Junior Backend Developer. Open Source Enthusiast 🔥. Always learning.",
-  resumeLink:
-    "https://drive.google.com/file/d/17HCKQFAcqZoSI9jTEsolAB_QuN0vhZSP/view?usp=sharing",
+  subTitle: "Expert Mobile Developer and Junior Backend Developer. Open Source Enthusiast 🔥. Always learning.",
+  resumeLink: "https://drive.google.com/file/d/17HCKQFAcqZoSI9jTEsolAB_QuN0vhZSP/view?usp=sharing",
   mail: "mailto:iranmanesh.ah@gmail.com",
 };
 
@@ -34,8 +32,7 @@ const socialMediaLinks = {
 };
 
 const skills = {
-  data: [
-    {
+  data: [{
       title: "Software Development",
       fileName: "FullStackImg",
       skills: [
@@ -46,8 +43,7 @@ const skills = {
         // "⚡ Adept at bringing all team members together to reach a common goal on time and under budget",
         // "⚡ Committed to viable and easily functional app solutions for clients",
       ],
-      softwareSkills: [
-        {
+      softwareSkills: [{
           skillName: "Android",
           fontAwesomeClassname: "simple-icons:android",
           style: {
@@ -226,21 +222,19 @@ const skills = {
 };
 
 const degrees = {
-  degrees: [
-    {
-      title: "Bachelor Degree of Computer Engineering",
-      subtitle: "Semnan University, Semnan, Iran",
-      logo_path: "semuni-logo.png",
-      alt_name: "BSC",
-      duration: "2016 - 2020",
-      descriptions: [
-        "⚡ I have studied core subjects like Data Structures, DBMS, Artificial intelligence (AI), Security, etc.",
-        "⚡ I have implemented several projects based on what I've leart under my Computer Engineering course. ",
-        "⚡ Teacher Assistant (TA) for 2 years in 2 C++ Programming Courses (Foundation and Advanced)",
-      ],
-      website_link: "https://ce.semnan.ac.ir/",
-    },
-  ],
+  degrees: [{
+    title: "Bachelor Degree of Computer Engineering",
+    subtitle: "Semnan University, Semnan, Iran",
+    logo_path: "semuni-logo.png",
+    alt_name: "BSC",
+    duration: "2016 - 2020",
+    descriptions: [
+      "⚡ I have studied core subjects like Data Structures, DBMS, Artificial intelligence (AI), Security, etc.",
+      "⚡ I have implemented several projects based on what I've leart under my Computer Engineering course. ",
+      "⚡ Teacher Assistant (TA) for 2 years in 2 C++ Programming Courses (Foundation and Advanced)",
+    ],
+    website_link: "https://ce.semnan.ac.ir/",
+  }, ],
 };
 
 const certifications = {
@@ -251,55 +245,46 @@ const certifications = {
 const experience = {
   title: "Experience",
   subtitle: "",
-  description:
-    "I've mostly worked as a freelancer mobile developer or done projects on my own. I love being creative and thinking about new ideas and projects. And now I am actively looking for a Golang backend developer position.",
+  description: "I've mostly worked as a freelancer mobile developer or done projects on my own. I love being creative and thinking about new ideas and projects. And now I am actively looking for a Golang backend developer position.",
   header_image_path: "experience.svg",
-  sections: [
-    {
+  sections: [{
       title: "Mobile Development",
-      experiences: [
-        {
-          title: "Freelancer",
-          company: "",
-          company_url: "",
-          logo_path: "freelancer.png",
-          duration: "2017 - Present",
-          location: "",
-          description: "Freelancer Android & Flutter developer",
-          color: "#743bff",
-        },
-      ],
+      experiences: [{
+        title: "Freelancer",
+        company: "",
+        company_url: "",
+        logo_path: "freelancer.png",
+        duration: "2017 - Present",
+        location: "",
+        description: "Freelancer Android & Flutter developer",
+        color: "#743bff",
+      }, ],
     },
     {
       title: "Web Development",
-      experiences: [
-        {
-          title: "Freelancer",
-          company: "",
-          company_url: "",
-          logo_path: "freelancer.png",
-          duration: "2020 - Present",
-          location: "",
-          description: "Freelancer backend developer in Golang & PHP",
-          color: "#743bff",
-        },
-      ],
+      experiences: [{
+        title: "Freelancer",
+        company: "",
+        company_url: "",
+        logo_path: "freelancer.png",
+        duration: "2020 - Present",
+        location: "",
+        description: "Freelancer backend developer in Golang & PHP",
+        color: "#743bff",
+      }, ],
     },
     {
       title: "Managerial",
-      experiences: [
-        {
-          title: "Thinker & CTO",
-          company: "IDEKR.co",
-          company_url: "https://idekr.com/",
-          logo_path: "idekr.jpeg",
-          duration: "2019 - Present",
-          location: "",
-          description:
-            "Thinker and Chief Technical Officer(CTO) in IDEKR at EiGHT company",
-          color: "#1d5a82",
-        },
-      ],
+      experiences: [{
+        title: "Thinker & CTO",
+        company: "IDEKR.co",
+        company_url: "https://idekr.com/",
+        logo_path: "idekr.jpeg",
+        duration: "2019 - Present",
+        location: "",
+        description: "Thinker and Chief Technical Officer(CTO) in IDEKR at EiGHT company",
+        color: "#1d5a82",
+      }, ],
     },
   ],
 };
@@ -309,13 +294,11 @@ const contactPageData = {
   contactSection: {
     title: "Contact Me",
     profile_image_path: "me.jpeg",
-    description:
-      "You can contact me at the places mentioned below. I will try to get back to you as fast as I can. ",
+    description: "You can contact me at the places mentioned below. I will try to get back to you as fast as I can. ",
   },
   blogSection: {
     title: "Blogs",
-    subtitle:
-      "I don't blog frequently but when I do something awesome, I do try to document it so it can be helpful to others. I write on Twitter.",
+    subtitle: "I don't blog frequently but when I do something awesome, I do try to document it so it can be helpful to others. I write on Twitter.",
     link: "",
     avatar_image_path: "blogs_image.svg",
   },
@@ -324,21 +307,36 @@ const contactPageData = {
 // Projects Page
 const projectsHeader = {
   title: "Projects",
-  description:
-    "My projects make use of a vast variety of latest technology tools. My best experience is to create Golang Backend Projects, Android Applications, and Flutter Projects. Below are some of my projects.",
+  description: "My projects make use of a vast variety of latest technology tools. My best experience is to create Golang Backend Projects, Android Applications, and Flutter Projects. Below are some of my projects.",
   avatar_image_path: "projects_image.svg",
 };
 
 const projects = {
-  data: [
+  data: [{
+      id: "6",
+      name: "Torob Search Telegram Bot",
+      url: "https://t.me/TorobSearchBot",
+      description: "A Telegram Bot that uses Torob API to search for product prices among the online shops and show the results.",
+      languages: [{
+          name: "MySQL",
+          iconifyClass: "logos-mysql",
+        },
+        {
+          name: "PHP",
+          iconifyClass: "logos-php",
+        }, 
+        {
+          name: "Telegram",
+          iconifyClass: "logos-telegram",
+        }, 
+      ],
+    },
     {
-      id: "0",
+      id: "5",
       name: "Radgir App",
       url: "https://radgir.net/",
-      description:
-        "Smart application to track post (and Tipax) packages with user-friendly Ui & featuring automation package tracking.",
-      languages: [
-        {
+      description: "Smart application to track post (and Tipax) packages with user-friendly Ui & featuring automation package tracking.",
+      languages: [{
           name: "Firebase",
           iconifyClass: "logos-firebase",
         },
@@ -357,38 +355,11 @@ const projects = {
       ],
     },
     {
-      id: "1",
+      id: "4",
       name: "HighFollower App",
       url: "https://highfollower.com/",
-      description:
-        "HighFollower is an Android application that helps to increase Instagram followers and interacting with Instagram APIs in the background.",
-      languages: [
-        {
-          name: "Firebase",
-          iconifyClass: "logos-firebase",
-        },
-        {
-          name: "Kotlin",
-          iconifyClass: "logos-kotlin",
-        },
-        {
-          name: "Java",
-          iconifyClass: "logos-java",
-        },
-        {
-          name: "Android",
-          iconifyClass: "logos-android",
-        },
-      ],
-    },
-    {
-      id: "2",
-      name: "Hesabchi App",
-      url: "https://hesabchiapp.ir/",
-      description:
-        "Hesabchi is a tool to manage daily costs and shares between roommates and teams.",
-      languages: [
-        {
+      description: "HighFollower is an Android application that helps to increase Instagram followers and interacting with Instagram APIs in the background.",
+      languages: [{
           name: "Firebase",
           iconifyClass: "logos-firebase",
         },
@@ -408,28 +379,17 @@ const projects = {
     },
     {
       id: "3",
-      name: "NerkhName App",
-      url: "https://b2n.ir/g18277",
-      description:
-        "Real-time Currency exchange price status Android application.",
-      languages: [
+      name: "Hesabchi App",
+      url: "https://hesabchiapp.ir/",
+      description: "Hesabchi is a tool to manage daily costs and shares between roommates and teams.",
+      languages: [{
+          name: "Firebase",
+          iconifyClass: "logos-firebase",
+        },
         {
           name: "Kotlin",
           iconifyClass: "logos-kotlin",
         },
-        {
-          name: "Android",
-          iconifyClass: "logos-android",
-        },
-      ],
-    },
-    {
-      id: "4",
-      name: "Zooril App",
-      url: "https://b2n.ir/p21933",
-      description:
-        "A smart Application for Zooril smart home kit's clients to control devices.",
-      languages: [
         {
           name: "Java",
           iconifyClass: "logos-java",
@@ -441,13 +401,41 @@ const projects = {
       ],
     },
     {
-      id: "5",
+      id: "2",
+      name: "NerkhName App",
+      url: "https://b2n.ir/g18277",
+      description: "Real-time Currency exchange price status Android application.",
+      languages: [{
+          name: "Kotlin",
+          iconifyClass: "logos-kotlin",
+        },
+        {
+          name: "Android",
+          iconifyClass: "logos-android",
+        },
+      ],
+    },
+    {
+      id: "1",
+      name: "Zooril App",
+      url: "https://b2n.ir/p21933",
+      description: "A smart Application for Zooril smart home kit's clients to control devices.",
+      languages: [{
+          name: "Java",
+          iconifyClass: "logos-java",
+        },
+        {
+          name: "Android",
+          iconifyClass: "logos-android",
+        },
+      ],
+    },
+    {
+      id: "0",
       name: "Aapks Store App",
       url: "https://aapks.com/",
-      description:
-        "AAPKS (Android APKs) is an App store application for android OS. It uses aptoide webservice.",
-      languages: [
-        {
+      description: "AAPKS (Android APKs) is an App store application for android OS. It uses aptoide webservice.",
+      languages: [{
           name: "Java",
           iconifyClass: "logos-java",
         },
