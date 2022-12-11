a20ca0b605c412d354f065ab00f07ac9cb90f6e8
shaan2727 <jslshashank27@gmail.com>
2022-12-11 14:48:30 +0000

    change the contact page

--- src/portfolio.js
+++ src/portfolio.js
@@ -544,16 +544,15 @@ const projectsHeader = {
 const contactPageData = {
   contactSection: {
     title: "Contact Me",
-    profile_image_path: "harikrushn.jpg",
+    profile_image_path: "icon1.png",
     description:
       "You can contact me at the places mentioned below. I will try to get back to you as fast as I can. ",
   },
   blogSection: {
     title: "Blogs",
-    subtitle:
-      "I don't blog frequently but when I do something awesome, I do try to document it so it can be helpful to others. I write on Twitter.",
-    link: "https://twitter.com/Harikrushn9",
-    avatar_image_path: "blogs_image.svg",
+    subtitle: "Blogging site will re-open soon",
+    link: "https://fastasturtle.com",
+    avatar_image_path: "",
   },
 };
 
